d0f8b912ed9364a9d9127c7ee99b3b648716cf35
bynrw <burak-yanik@hotmail.de>
2025-04-07 11:14:34 +0200

    api-micros1u2

---
+++ .history/api-gateway/api-gateway/src/main/java/com/example/api_gateway/DemoApplication_20250407110532.java
@@ -0,0 +1,18 @@
+package com.example.api_gateway;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.openfeign.EnableFeignClients;		
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
